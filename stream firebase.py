8e0fee200d1d1ea4debd03e92c6bcb80e3b7138d
Prerna Khanna <35433654+prerna-khanna@users.noreply.github.com>
2018-10-04 20:10:09 +0530

    Add files via upload

---
+++ stream firebase.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from urllib.request import urlopen
+import pyrebase
+import os
+import features
+import nearest_loc
+import time
+
+config = {
+  "apiKey": "AIzaSyDwLt60kJLcjgLCET9KDyyj6fx2rmi1EZQ",
+  "authDomain": "airmirror-final.firebaseapp.com",
+  "databaseURL": "https://airmirror-final.firebaseio.com/",
+  "storageBucket": "airmirror-final.appspot.com",
+  #"serviceAccount": "path/to/serviceAccountCredentials.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+st = firebase.storage()
+
+def stream_handler(message):    
+    print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+    entry_len = len(message["path"])
+    entry = message["path"]
+    if(message and message['data'] != None):
+        if(entry_len >1 and (entry[entry_len-1]+ entry[entry_len-2] + entry[entry_len-3] != 'mP/')):
+            newpath = r"C:\Users\HP\Desktop\%s" %message["path"]
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
+                time.sleep(2)
+            st.child("uploads/air/394.jpg").download(r"C:\Users\HP\Desktop\%s\1.jpg"%message["path"])            
+            features.main()
+            nearest_loc.main()
+my_stream = db.stream(stream_handler, stream_id="new_posts")
