9960ad7ffe73316bd113eded16442e4ceaa2ea1c
Prerna <prerna.khanna97@gmail.com>
2018-10-04 17:06:41 +0530

    first

---
+++ app/src/main/java/com/example/hp/superwiemen/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.hp.superwiemen;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static DatabaseReference mDatabaseRef;
+    private static String uniqueID = null;
+    private static final String PREF_UNIQUE_ID = "PREF_UNIQUE_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+        SharedPreferences sharedPrefs = this.getSharedPreferences(
+                PREF_UNIQUE_ID, Context.MODE_PRIVATE);
+        uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null);
+        if (uniqueID == null) {
+            uniqueID = UUID.randomUUID().toString();
+            SharedPreferences.Editor editor = sharedPrefs.edit();
+            editor.putString(PREF_UNIQUE_ID, uniqueID);
+            editor.commit();
+        }
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if(!prefs.getBoolean("firstTime", false)) {
+            // run your one time code
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            mDatabaseRef.child(String.valueOf(uniqueID)).child("flag").setValue(1);
+
+            Intent intent = new Intent(MainActivity.this, details.class);
+            startActivity(intent);
+
+            editor.commit();
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/hp/superwiemen/details.java
@@ -0,0 +1,19 @@
+package com.example.hp.superwiemen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class details extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+        System.out.println("here");
+
+
+
+    }
+
+}
